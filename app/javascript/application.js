00ac0ca0aa0efe90ca3aa03bdc4d6cc252d07963
kinginthenorthcodez <kinginthenorthcodez@gmail.com>
2022-09-26 21:26:11 +0530

    Fix eslint error

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,16 +1,16 @@
 // Entry point for the build script in your package.json
 // import "@hotwired/turbo-rails";
 // import "./controllers";
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import { Provider } from "react-redux";
-import store from "./redux/store";
-import { fetchGreet } from "./redux/messageReducer";
-import App from "./components/App";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './redux/store';
+import { fetchGreet } from './redux/messageReducer';
+import App from './components/App';
 
 store.dispatch(fetchGreet());
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
