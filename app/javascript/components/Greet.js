42c38988b5055850e4ff4af60b9985e4318522d5
Kinginthenorthcodez <kinginthenorthcodez@gmail.com>
2022-09-26 21:33:02 +0530

    Update Greet.js

--- app/javascript/components/Greet.js
+++ app/javascript/components/Greet.js
@@ -5,8 +5,8 @@ function Greet() {
   const message = useSelector((state) => state);
   return (
     <>
-      <div className='App'>
-        <header className='App-header'>
+      <div className="App">
+        <header className="App-header">
           <p style={{ fontSize: 26 }}>
             Message:
             {message.greet}
